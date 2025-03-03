efad9445c7e386a9f7f909ec5e65ee04f76cb312
Reem-Front-End <122124103+Reem-Elamoudi@users.noreply.github.com>
2025-03-03 09:39:13 +0200

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+function Contact() {
+  return (
+<section className="contact">
+   frome contact
+</section>
+   );
+}
+
+export default Contact;
