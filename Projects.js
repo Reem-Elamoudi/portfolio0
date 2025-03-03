8b56c6891f99450a00d5e9b2f57579e504822835
Reem-Front-End <122124103+Reem-Elamoudi@users.noreply.github.com>
2025-03-03 09:36:56 +0200

    Create Projects.js

---
+++ Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+function Home() {
+  return (
+<section className="projects">
+   frome projects
+</section>
+   );
+}
+
+export default Projects;
