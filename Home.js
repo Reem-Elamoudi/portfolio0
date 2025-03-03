bfe81737f14a80b8c4812e8ce294070732baf247
Reem-Front-End <122124103+Reem-Elamoudi@users.noreply.github.com>
2025-03-03 09:33:44 +0200

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+function Home() {
+  return (
+<section className="home">
+   frome home
+</section>
+   );
+}
+
+export default Home;
