6c4dc56bf9517e9b84166293dfad1edc036e81ea
Reem-Front-End <122124103+Reem-Elamoudi@users.noreply.github.com>
2025-03-03 09:35:44 +0200

    Create Skills.js

---
+++ Skills.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+function Skills() {
+  return (
+<section className="home">
+   frome skills
+</section>
+   );
+}
+
+export default Skills;
